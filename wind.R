b1decacba3f5cff619b390c00e58cfb8871a265f
Saptarshi Das <arkaforyou.1611@gmail.com>
2019-12-09 12:49:47 +0000

    Finish

--- wind.R
+++ wind.R
@@ -24,141 +24,11 @@ library(dplyr)
 setwd("C:/Users/HP/Downloads/Research Project/Data")
 
 
-#LOAD DATA
-TN <- read.xlsx("TN_Chennai_Alandur.xlsx", 1)
-TN = separate(TN, col = To.Date, into = c("Date"), sep = 10, remove=T) #tidyr
-
-TN$Date=strptime(TN$Date, format= "%d-%m-%Y") #convert factor to POSIXlt time object
-#convert factor to numeric
-TN$WS = as.character(TN$WS)
-TN$WS = as.numeric(TN$WS)
-#creating a zoo object
-temp= zoo(TN %>% select(2), order.by = TN$Date) #dplyr#zoo
-#replacing the outliers and null values with locally smoothed values
-temp$WS <- tsclean(temp$WS) #forecast
-
-#agregating the hourly data to daily format in oder to make mid-term predictions successfully
-TN_ag= aggregate(temp, as.Date, mean)
-TN_ag[,1]=round(TN_ag[,1],digits = 2)
-
-TN.ts <- ts(TN_ag, start= c(2016, 01, 01), frequency=365)
-TN.df <- as.data.frame(TN_ag)
-names(TN.df)[1] <- "y"
-write.table(TN.df, "C:/Users/HP/Downloads/Research Project/Data/TN_all.csv", sep=" ,", row.names=FALSE)
-autoplot(decompose(TN.ts))
-
-autoplot(TN.ts) +
-  ggtitle("Wind Speed data of Tamil Nadu(2016-2019)") +
-  xlab("Year") +
-  ylab("Meter/Second") #ggplot2
-
-#Dickey-Fuller test for Stationarity
-apply(TN.ts, 2, adf.test) #tseries
-
-
-#Ljung-Box Test for white noise
-Box.test(TN.ts, lag = 24, fitdf = 0, type = "Ljung")
-
-# The pvalue very less than 0.05 suggests that the data is not white noise
-
-#spliting 75% of the original data for training the models
-
-Tamil<-splitTrainTest(TN.ts, numTrain = length(TN.ts) - 354) #CombMSC
-
-#checking linearity
-ggqqplot(TN.df$y)+
-  ggtitle("TamilNadu Data Linearity")
-#Shapiro-Wilk normality test
-shapiro.test(TN.df$y) 
-##though from the qqplot the data seems a bit non-linear, but from the 
-#p value <0.05 of Shapiro-Wilk normality test, it can be said that the data is normally distributed
-#As the data is normally distributed, BoxCox transformation is not required
-
-
-######################################## ARIMA ##########################################
-
-TNarima <-auto.arima(Tamil$train,lambda=NULL,stationary=T, 
-                     stepwise=F, trace=F, approximation=F, biasadj=F)
-##Forecast with model
-TNarima.forecast <-forecast(TNarima,h=354)
-##Plot the forecast
-autoplot(TNarima.forecast) + ylab("Wind Speed") 
-##Accuracy store
-TNarima.acc<-as.data.frame(accuracy(TNarima.forecast))
-checkresiduals(TNarima)
-
-#################################### Neural Net ###########################################
-set.seed(18127355)
-TNnn=nnetar(Tamil$train,p=41,P=2,size=19, repeats=50, lambda = NULL, scale.inputs=F)
-
-TNnn.forecast=forecast(TNnn, PI=F,h=354)
-TNnn.acc<-data.frame(accuracy(TNnn.forecast))
-
-autoplot(TNnn.forecast) + ylab("Wind Speed")
-
-########################## Simple Exponential Smoothening ################################
-
-TNses <- ses(Tamil$train, h = 354, lambda = NULL, initial="optimal", biasadj=F)
-
-# checking the error rates to evaluate the model
-TNses.acc<-data.frame(accuracy(TNses))
-
-# Add the one-step forecasts for the training data to the plot
-autoplot(Tamil$train) + autolayer(fitted(TNses), series = "ses") + ylab("Wind Speed")
-
-################################ Prophet Model ##############################################
-library(Metrics)
-TN.temporal <- read.csv("TN_all.csv")
-
-TN.prophet<-prophet(TN.temporal[1:1062,], growth = 'linear', changepoints =c("2016-12-12"),
-                    seasonality.mode = 'additive', daily.seasonality=F, fit = T)
-TNfuture <- make_future_dataframe(TN.prophet, periods = 354)
-TN.prophetforecast <- predict(TN.prophet, TNfuture, type="response")
-
-dyplot.prophet(TN.prophet, TN.prophetforecast)
-
-####### Errors
-
-TNactual<- TN.temporal[1063:1416,2]
-
-
-TNprophet.acc<-data.frame(rmse=rmse(TNactual, TN.prophetforecast$yhat), mae=mae(TNactual, TN.prophetforecast$yhat), mape=mape(TNactual, TN.prophetforecast$yhat))
-
-detach("package:Metrics", unload = TRUE)
-
-
-####################Dynamic Harmonic Regression###################
-
-TNdhr<- auto.arima(Tamil$train, xreg= fourier(Tamil$train, K=3), lambda = 0, 
-                   stepwise=F, trace=F, approximation=F, biasadj=F)
-
-summary(TNdhr) # checking the AICc value from summary of the fit model, the value of K is fixed with the minimum AICc value
-TNdhr.forecast<-forecast(TNdhr, xreg= fourier(TN.ts, K=3, h=354))
-TNdhr.acc<-accuracy(TNdhr.forecast)
-
-autoplot(TNdhr.forecast) + ylab("Wind Speed")
-
-############################################################################################################
-############### Accuracy Table ######################
-TN.evaluation_table <- data.frame("Models" = c("ARIMA","NN", "SES", "PROPHET", "DHR"), "RMSE" =0, "MAE" =0, "MAPE"=0)
-
-
-TN.evaluation_table[1,2:4]<-TNarima.acc[1,c(2,3,5)]
-TN.evaluation_table[2,2:4] <- TNnn.acc[1,c(2,3,5)]
-TN.evaluation_table[3,2:4] <- TNses.acc[1,c(2,3,5)]
-TN.evaluation_table[4,2:4] <- TNprophet.acc[1,1:3]
-TN.evaluation_table[5,2:4] <- TNdhr.acc[1,c(2,3,5)]
-write.table(TN.evaluation_table, "C:/Users/HP/Downloads/Research Project/Data/TN_accuracy.csv", sep=" ,", row.names=FALSE)
-
-######################################################################################################################
-###################################################################################################################################
 #############################################################################################################################
 ##########################################################################################################################
 ###########################################    Maharashtra     ##############################################################################
 #################################################################################################################################
 #########################################################################################################################
-###############################################################################################################################
-######################################################################################################################
 
 
 MH <- read.xlsx("Maharashtra_Chandrapur.xlsx", 1)
@@ -223,7 +93,7 @@ autoplot(MHarima.forecast) + ylab("Wind Speed")
 
 MHarima.acc<-accuracy(MHarima.forecast)
 
-####################### Neural Net ########################
+####################### Neural Net ###################################################
 set.seed(18127355)
 MHnn=nnetar(Maharashtra$train,p=41,P=2,size =19,repeats=50, lambda = NULL, scale.inputs=F)
 
@@ -234,7 +104,7 @@ MHnn.acc<-accuracy(MHnn.forecast)
 autoplot(MHnn.forecast) + ylab("Wind Speed")
 
 
-################### Simple Exponential Smoothening ####################
+################### Simple Exponential Smoothening ##########################################
 
 MHses <- ses(Maharashtra$train, h = 354, lambda = NULL, initial="optimal", biasadj=F)
 
@@ -245,7 +115,7 @@ MHses.acc<-accuracy(MHses)
 # Add the one-step forecasts for the training data to the plot
 autoplot(Maharashtra$train) + autolayer(fitted(MHses), series = "ses") + ylab("Wind Speed")
 
-########################## Prophet Model #############
+########################## Prophet Model #############################################################
 library(Metrics)
 MH.temporal <- read.csv("MH_all.csv")
 
@@ -258,7 +128,7 @@ dyplot.prophet(MH.prophet, MH.prophetforecast)
 ####### Errors
 MHactual<- MH.temporal[1063:1416,2]
 
-MHprophet.acc<-data.frame(rmse=rmse(MHactual, MH.prophetforecast$yhat), mae=mae(MHactual, MH.prophetforecast$yhat), mape=mape(MHactual, MH.prophetforecast$yhat))
+MHprophet.acc<-data.frame(rmse=rmse(MHactual, MH.prophetforecast$yhat), mae=mae(MHactual, MH.prophetforecast$yhat))
 detach("package:Metrics", unload = TRUE)
 
 
@@ -271,43 +141,184 @@ MHdhr.forecast<-forecast(MHdhr, xreg= fourier(MH.ts, K=1, h=354))
 MHdhr.acc<-accuracy(MHdhr.forecast)
 checkresiduals(MHdhr)
 autoplot(MHdhr.forecast) + ylab("Wind Speed")
+
 ############################################################################################################
 ############### Accuracy Table ######################
-MH.evaluation_table <- data.frame("Models" = c("ARIMA","NN", "SES", "PROPHET", "DHR"), "RMSE" =0, "MAE" =0, "MAPE"=0)
+MH.evaluation_table <- data.frame("Models" = c("ARIMA","NN", "SES", "PROPHET", "DHR"), "RMSE" =0, "MAE" =0)
 
-MH.evaluation_table[1,2:4] <- MHarima.acc[1,c(2,3,5)]
-MH.evaluation_table[2,2:4] <- MHnn.acc[1,c(2,3,5)]
-MH.evaluation_table[3,2:4] <- MHses.acc[1,c(2,3,5)]
-MH.evaluation_table[4,2:4] <- MHprophet.acc[1,1:3]
-MH.evaluation_table[5,2:4] <- MHdhr.acc[1,c(2,3,5)]
+MH.evaluation_table[1,2:3] <- MHarima.acc[1,c(2,3)]
+MH.evaluation_table[2,2:3] <- MHnn.acc[1,c(2,3)]
+MH.evaluation_table[3,2:3] <- MHses.acc[1,c(2,3)]
+MH.evaluation_table[4,2:3] <- MHprophet.acc[1,1:2]
+MH.evaluation_table[5,2:3] <- MHdhr.acc[1,c(2,3)]
 write.table(MH.evaluation_table, "C:/Users/HP/Downloads/Research Project/Data/MH_accuracy.csv", sep=" ,", row.names=FALSE)
+
+
+
+################################################################################################################################
+######################################################################################################################
+######################################## Tamil Nadu #################################################################
+#######################################################################################################################
+########################################################################################################################
+
+#LOAD DATA
+TN <- read.xlsx("TN_Chennai_Alandur.xlsx", 1)
+TN = separate(TN, col = To.Date, into = c("Date"), sep = 10, remove=T) #tidyr
+
+TN$Date=strptime(TN$Date, format= "%d-%m-%Y") #convert factor to POSIXlt time object
+#convert factor to numeric
+TN$WS = as.character(TN$WS)
+TN$WS = as.numeric(TN$WS)
+#creating a zoo object
+temp= zoo(TN %>% select(2), order.by = TN$Date) #dplyr#zoo
+#replacing the outliers and null values with locally smoothed values
+temp$WS <- tsclean(temp$WS) #forecast
+
+#agregating the hourly data to daily format in oder to make mid-term predictions successfully
+TN_ag= aggregate(temp, as.Date, mean)
+TN_ag[,1]=round(TN_ag[,1],digits = 2)
+
+TN.ts <- ts(TN_ag, start= c(2016, 01, 01), frequency=365)
+TN.df <- as.data.frame(TN_ag)
+names(TN.df)[1] <- "y"
+write.table(TN.df, "C:/Users/HP/Downloads/Research Project/Data/TN_all.csv", sep=" ,", row.names=FALSE)
+autoplot(decompose(TN.ts))
+
+autoplot(TN.ts) +
+  ggtitle("Wind Speed data of Tamil Nadu(2016-2019)") +
+  xlab("Year") +
+  ylab("Meter/Second") #ggplot2
+
+#Dickey-Fuller test for Stationarity
+apply(TN.ts, 2, adf.test) #tseries
+
+
+#Ljung-Box Test for white noise
+Box.test(TN.ts, lag = 24, fitdf = 0, type = "Ljung")
+
+# The pvalue very less than 0.05 suggests that the data is not white noise
+
+#spliting 75% of the original data for training the models
+
+Tamil<-splitTrainTest(TN.ts, numTrain = length(TN.ts) - 354) #CombMSC
+
+#checking linearity
+ggqqplot(TN.df$y)+
+  ggtitle("TamilNadu Data Linearity")
+#Shapiro-Wilk normality test
+shapiro.test(TN.df$y) 
+##though from the qqplot the data seems a bit non-linear, but from the 
+#p value <0.05 of Shapiro-Wilk normality test, it can be said that the data is normally distributed
+#As the data is normally distributed, BoxCox transformation is not required
+
+
+######################################## ARIMA ##########################################
+
+TNarima <-auto.arima(Tamil$train,lambda=NULL,stationary=T, 
+                     stepwise=F, trace=F, approximation=F, biasadj=F)
+##Forecast with model
+TNarima.forecast <-forecast(TNarima,h=354)
+##Plot the forecast
+autoplot(TNarima.forecast) + ylab("Wind Speed") 
+##Accuracy store
+TNarima.acc<-as.data.frame(accuracy(TNarima.forecast))
+checkresiduals(TNarima)
+
+#################################### Neural Net ###########################################
+
+TNnn=nnetar(Tamil$train,p=41,P=2,size=19, repeats=50, lambda = NULL, scale.inputs=F)
+
+TNnn.forecast=forecast(TNnn, PI=F,h=354)
+TNnn.acc<-data.frame(accuracy(TNnn.forecast))
+
+autoplot(TNnn.forecast) + ylab("Wind Speed")
+
+########################## Simple Exponential Smoothening ################################
+
+TNses <- ses(Tamil$train, h = 354, lambda = NULL, initial="optimal", biasadj=F)
+
+# checking the error rates to evaluate the model
+TNses.acc<-data.frame(accuracy(TNses))
+
+# Add the one-step forecasts for the training data to the plot
+autoplot(Tamil$train) + autolayer(fitted(TNses), series = "ses") + ylab("Wind Speed")
+
+################################ Prophet Model ##############################################
+library(Metrics)
+TN.temporal <- read.csv("TN_all.csv")
+
+TN.prophet<-prophet(TN.temporal[1:1062,], growth = 'linear', changepoints =c("2016-12-12"),
+                    seasonality.mode = 'additive', daily.seasonality=F, fit = T)
+TNfuture <- make_future_dataframe(TN.prophet, periods = 354)
+TN.prophetforecast <- predict(TN.prophet, TNfuture, type="response")
+
+dyplot.prophet(TN.prophet, TN.prophetforecast)
+
+####### Errors
+
+TNactual<- TN.temporal[1063:1416,2]
+
+
+TNprophet.acc<-data.frame(rmse=rmse(TNactual, TN.prophetforecast$yhat), mae=mae(TNactual, TN.prophetforecast$yhat))
+
+detach("package:Metrics", unload = TRUE)
+
+
+####################Dynamic Harmonic Regression###################
+
+TNdhr<- auto.arima(Tamil$train, xreg= fourier(Tamil$train, K=3), lambda = 0, 
+                   stepwise=F, trace=F, approximation=F, biasadj=F)
+
+summary(TNdhr) # checking the AICc value from summary of the fit model, the value of K is fixed with the minimum AICc value
+TNdhr.forecast<-forecast(TNdhr, xreg= fourier(TN.ts, K=3, h=354))
+TNdhr.acc<-accuracy(TNdhr.forecast)
+
+autoplot(TNdhr.forecast) + ylab("Wind Speed")
+
+############################################################################################################
+############### Accuracy Table ######################
+TN.evaluation_table <- data.frame("Models" = c("ARIMA","NN", "SES", "PROPHET", "DHR"), "RMSE" =0, "MAE" =0)
+
+
+TN.evaluation_table[1,2:3]<-TNarima.acc[1,c(2,3)]
+TN.evaluation_table[2,2:3] <- TNnn.acc[1,c(2,3)]
+TN.evaluation_table[3,2:3] <- TNses.acc[1,c(2,3)]
+TN.evaluation_table[4,2:3] <- TNprophet.acc[1,1:2]
+TN.evaluation_table[5,2:3] <- TNdhr.acc[1,c(2,3)]
+write.table(TN.evaluation_table, "C:/Users/HP/Downloads/Research Project/Data/TN_accuracy.csv", sep=" ,", row.names=FALSE)
+
 
 #################################################################################################################################################
 ##########################################################################################################################
 ######################################## Forecasting Wind power########################################################
+###########################################################################################################################
 
 #NN performed the best in handling both the location's wind speed data and forecasting it
 #so now 180 days of future wind speed will be forecasted by NN
 
-TNfit=nnetar(TN.ts,p=41,P=3,size = 19,repeats=50, lambda = TNlambda, scale.inputs=T)
+TNfit=nnetar(TN.ts,p=41,P=3,size = 19,repeats=50, lambda = NULL, scale.inputs=T)
 TNWS.forecast=forecast(TNfit, h=180)
-  
-MHfit=nnetar(MH.ts,p=41,P=3,size =19 ,repeats=50, lambda = MHlambda, scale.inputs=TRUE)
+
+
+MHfit=nnetar(MH.ts,p=41,P=3,size =19 ,repeats=50, lambda = NULL, scale.inputs=TRUE)
 MHWS.forecast=forecast(MHfit, h=180)
 
 ########################################################################################################################
 
-##########Theoritically Calculating Wind Power with probable parameter values
+##########Theoritically Calculating Wind Power with probable parameter values##################################
 
 ##############################################################################################################
 #WP=0.5*Cp*q*A*V^3 
 #[Cp=Max power coefficient(theoretical max-> 0.59)]
-#[q=air density(1.25kg/m^3 at sea level)] 
-#[A=Swept area of the rotor (largest rotor used in India for commercial onshore turbines are of 60m, so swept area=12470m^2) ]
+#[q=air density(1.225kg/m^3 at sea level)] 
+#[A=Swept area of the rotor (largest rotor diameter used in the current time is 129m, so swept area=13070m^2) ]
 #[V=Wind Speed(m/s), which in this case have been forecasted]
-FutureWP<-data.frame("TNWP"=(0.5 * 0.59 * 1.25 * 2827.44* TNWS.forecast$mean^3), "MHWP"=(0.5 * 0.59 * 1.25 * 2827.44 * MHWS.forecast$mean^3))
+FutureWP<-data.frame("TamilWP"=(0.5 * 0.59 * 1.225 * 13070 * TNWS.forecast$mean^3), 
+                     "MaharashtraWP"=(0.5 * 0.59 * 1.225 * 13070 * MHWS.forecast$mean^3))
 
 write.table(FutureWP, "C:/Users/HP/Downloads/Research Project/Data/Future_WP.csv", sep=" ,", row.names=FALSE)
 
-#autoplot(as.ts(FutureWP, start=c(2019, 11, 17), frequency=365))+ggtitle("Future Wind Power Produced in the 2 States") +xlab("Time") +ylab("MW")
+autoplot(as.ts(FutureWP, start=c(2019, 11, 17), frequency=365))+ggtitle("Future Wind Power Produced in the 2 States") +xlab("Time") +ylab("KW")
+
+
 
